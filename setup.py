fb44b1cc7b7b7ed739b0c608fe7ef4d47d5c12fc
Hiroki Kiyohara <hiroki.kiyohara@beproud.jp>
2013-06-26 14:01:27 +0900

    Improved the setup.py containing more information.

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name='sentry-argstagging',
     version='0.0.1',
+    description='Sentry Pulgin to set tags by using arguments of Message event.',
     author='Hiroki KIYOHARA',
     author_email='hirokiky@gmail.com',
+    url='https://github.com/hirokiky/sentry_argstagging'
     classifiers=[
       'Development Status :: 1 - Planning',
       'Environment :: Plugins',
